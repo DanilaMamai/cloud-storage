d5dfc6a22fee71e9ff360a15cc6d3f73b6bed6ee
Мамаенко Даниил Евгеньевич <de_mamaenko@betboom.org>
2024-09-16 21:11:52 +0300

    main -added: init migration

---
+++ cloud-api/migrations/1726509859014-auto.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface } from "typeorm";
+
+export class Auto1726509859014 implements MigrationInterface {
+  public async up(): Promise<void> {}
+
+  public async down(): Promise<void> {}
+}
---
+++ cloud-api/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { DataSource } from "typeorm";
+
+config();
+
+export default new DataSource({
+  type: "postgres",
+  host: process.env.TYPEORM_HOST,
+  port: Number(process.env.TYPEORM_PORT),
+  database: process.env.TYPEORM_DATABASE,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  migrations: ["migrations/**"],
+  entities: [],
+});
