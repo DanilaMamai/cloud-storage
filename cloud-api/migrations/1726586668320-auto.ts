e6712110ea5d1faebbd8f8394c59ae2d76d8ea6c
Мамаенко Даниил Евгеньевич <de_mamaenko@betboom.org>
2024-09-17 18:25:20 +0300

    main -added: files migration

--- cloud-api/migrations/1726586668320-auto.ts
+++ cloud-api/migrations/1726586668320-auto.ts
@@ -1,9 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Auto1726514906817 implements MigrationInterface {
-  name = "Auto1726514906817";
+export class Auto1726586668320 implements MigrationInterface {
+  name = "Auto1726586668320";
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "files" ("id" SERIAL NOT NULL, "fileName" character varying NOT NULL, "size" integer NOT NULL, "mimetype" character varying NOT NULL, "deletedAt" TIMESTAMP, CONSTRAINT "PK_6c16b9093a142e0e7613b04a3d9" PRIMARY KEY ("id"))`,
+    );
     await queryRunner.query(
       `CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "firstName" character varying NOT NULL, "surName" character varying NOT NULL, "middleName" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
     );
@@ -11,5 +14,6 @@ export class Auto1726514906817 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`DROP TABLE "users"`);
+    await queryRunner.query(`DROP TABLE "files"`);
   }
 }
